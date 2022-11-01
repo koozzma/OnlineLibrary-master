23469230297a218da810bc54e5dc4076882538fe
Bogdan <51080321+koozzma@users.noreply.github.com>
2022-11-01 15:09:48 +0300

    Update SpringConfig.java

--- src/main/java/ru/project/config/SpringConfig.java
+++ src/main/java/ru/project/config/SpringConfig.java
@@ -24,9 +24,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-/**
- * @author Neil Alishev
- */
+
 @Configuration
 @ComponentScan("ru.project")
 @PropertySource("classpath:hibernate.properties")
